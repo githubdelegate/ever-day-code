ee3a9a8eec16e011b8f5d637290d2de050dcde03
zy <1722413885@qq.com>
2015-02-01 16:46:24 +0800

    demo

---
+++ demo/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  demo
+//
+//  Created by zy on 14/12/24.
+//  Copyright (c) 2014年 zy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+   
+    NSMutableArray *array = [NSMutableArray array];
+    [array insertObject:nil atIndex:0];
+    
+}
+
+@end
